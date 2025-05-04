6ac8269f976c28697190d7e57ac459125f786967
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2025-05-04 17:18:37 -0600

    Add win and lose conditions; export variables to the editor

--- player.gd
+++ player.gd
@@ -1,11 +1,38 @@
 extends RigidBody3D
 
 
+## How much vertical force to apply when moving
+@export_range(75.0, 3_000.0) var thrust: float = 1_000.0
+
+## How much rotation force to apply while moving
+@export var torque: float = 100.0
+
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * 1_000.0)
+		apply_central_force(basis.y * delta * thrust)
 
 	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, torque * delta))
 	elif Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, -torque * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		print("You've won!")
+		complete_level()
+	elif "Start" in body.get_groups():
+		print("Starting position set!")
+	elif "Hazard" in body.get_groups():
+		crash_sequence()
+
+
+func crash_sequence() -> void:
+	get_tree().call_deferred("reload_current_scene")
+	get_tree().quit()
+
+
+
+func complete_level() -> void:
+	get_tree().quit()
